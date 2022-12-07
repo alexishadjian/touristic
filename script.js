7fc1ef974f4d0f0d08eb1529c4b4911dd31bfb64
unknown <alexishadjian@gmail.com>
2022-12-07 23:20:27 +0100

    Weather, contact form

--- script.js
+++ script.js
@@ -68,17 +68,16 @@ jQuery(document).ready(function($){
     Map
     --------------------------------------------------------------*/ 
 
-    var map = L.map('map').setView([-4.279582929517693, 122.87303532649939], 10);
+    var map = L.map('leaflet-map').setView([-16.689399012363168, 179.21954740180354], 10);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
 
-    var marker = L.marker([-4.279582929517693, 122.87303532649939]).addTo(map);
+    var marker = L.marker([-16.689399012363168, 179.21954740180354]).addTo(map);
 
-    marker.bindPopup("<b>Indonésie</b><br>Des îles perdues.").openPopup();
+    marker.bindPopup("<b>Îles Fidji, Océanie</b><br>Des îles perdues").openPopup();
     
 });
 
@@ -87,19 +86,49 @@ jQuery(document).ready(function($){
 Météo
 --------------------------------------------------------------*/ 
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-//     let url = "https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=03bf3f0c6d128147c1d3bcfb91919c39&units=metric";
+    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fidji?unitGroup=metric&key=WALJ29LGHSBP7G8C9VW6M5B68&contentType=json";
   
-//     fetch(url)
-//       .then( response => {
-//           return response.json();
-//         }
-//       )
-//       .then(data => {
-//         console.log(data)
-//         document.querySelector('h1').innerHTML = data.main.temp + '°C';
-  
-//     })
+    fetch(url)
+      .then( response => {
+          return response.json();
+        }
+      )
+      .then(data => {
+        console.log(data)
 
-// })
+        // Clear icons class
+        document.querySelector('.weather span').removeAttribute('class'); 
+
+        // Current informations
+        document.querySelector('.weather .weather__current .temp').innerHTML = data.currentConditions.temp.toFixed(0) + '°';
+        document.querySelector('.weather .weather__current .place').innerHTML = data.resolvedAddress;
+        document.querySelector('.weather .weather__current .desc').innerHTML = data.days[0].conditions;
+        document.querySelector('.weather .weather__current span').classList.add(data.days[0].icon);
+
+        // Forecast informations
+        document.querySelector('.weather .weather__forecast__single.first .temp').innerHTML = data.days[1].temp.toFixed(0) + '°';
+        document.querySelector('.weather .weather__forecast__single.second .temp').innerHTML = data.days[2].temp.toFixed(0) + '°';
+        document.querySelector('.weather .weather__forecast__single.third .temp').innerHTML = data.days[3].temp.toFixed(0) + '°';
+        document.querySelector('.weather .weather__forecast__single.first span ').classList.add(data.days[1].icon);
+        document.querySelector('.weather .weather__forecast__single.second span ').classList.add(data.days[2].icon);
+        document.querySelector('.weather .weather__forecast__single.third span ').classList.add(data.days[3].icon);
+    })
+
+    let daysNumber = new Date().getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    days.forEach( ( day,index ) => {
+        // Check if the index of day value is equal to the returned value of getDay()
+        if ( index == daysNumber + 1 ) {
+            document.querySelector('.weather .weather__forecast__single.first .day').innerHTML = day;
+        } else if ( index == daysNumber + 2 ) {
+            document.querySelector('.weather .weather__forecast__single.second .day').innerHTML = day;
+        } else if ( index == daysNumber + 3 ) {
+            document.querySelector('.weather .weather__forecast__single.third .day').innerHTML = day;
+        }
+
+    });
+
+})
